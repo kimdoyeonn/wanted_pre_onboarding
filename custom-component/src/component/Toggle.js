80eb35839a6f61c5dde0f8bad6b2e54c933f15df
doyeon <doyeon311@gmail.com>
2022-02-01 01:06:49 +0900

    Feat: toggle

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Toggle = () => {
-  return <></>;
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <div className='w-full h-full flex items-center justify-center flex-col'>
+      <div
+        onClick={handleToggle}
+        className={`relative h-9 w-20 mb-3 shadow-inner bg-gray-300 rounded-3xl flex items-center cursor-pointer overflow-hidden`}
+      >
+        <div
+          className={`absolute h-full w-full ${
+            toggle ? 'left-0' : '-left-20'
+          } bg-violet-800 transition-all`}
+        ></div>
+        <div
+          className={`absolute ${
+            toggle ? 'left-11' : 'left-0'
+          } transition-all h-7 w-7 ml-1 bg-white rounded-full shadow-sm`}
+        ></div>
+      </div>
+      <div>Toggle Switch {toggle ? 'ON' : 'OFF'}</div>
+    </div>
+  );
 };
 
 export default Toggle;
+
+// const Back = styled.div`
+//   background: #c5c5c5;
+//   width: 5rem;
+//   height: 2.4rem;
+//   border-radius: 1.2rem;
+//   margin-bottom: 0.5rem;
+// `;
