584d475f1ef7b1ae5569d39a3cdc523d79be6648
doyeon <doyeon311@gmail.com>
2022-02-01 23:14:55 +0900

    Feat: Tab 기능 구현

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AutoComplete from './component/AutoComplete';
 import ClickToEdit from './component/ClickToEdit';
 import Modal from './component/Modal';
+import Tab from './component/Tab';
 import Toggle from './component/Toggle';
 
 function App() {
@@ -12,6 +13,14 @@ function App() {
         <Toggle />
       </div>
       <div className='container'>
+        <div className='title'>Modal</div>
+        <Modal />
+      </div>
+      <div className='container'>
+        <div className='title'>Tab</div>
+        <Tab />
+      </div>
+      <div className='container'>
         <div className='title'>AutoComplete</div>
         <AutoComplete />
       </div>
@@ -19,27 +28,8 @@ function App() {
         <div className='title'>ClickToEdit</div>
         <ClickToEdit />
       </div>
-      <div className='container'>
-        <div className='title'>Modal</div>
-        <Modal />
-      </div>
     </div>
   );
 }
 
 export default App;
-
-// const Container = styled.div`
-//   width: 100%;
-//   height: 20rem;
-//   border-radius: 1rem;
-//   border: 3px solid #ddd;
-//   margin: 1rem;
-// `;
-
-// const Title = styled.div`
-//   font-size: 1.5rem;
-//   font-weight: 700;
-//   padding-left: 1rem;
-//   padding-top: 0.3rem;
-// `;
---
+++ custom-component/src/component/Tab.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const Tab = () => {
+  const [select, setSelect] = useState(0);
+  const contents = [
+    { name: 'Tab1', message: 'Tab menu ONE' },
+    { name: 'Tab2', message: 'Tab menu TWO' },
+    { name: 'Tab3', message: 'Tab menu THREE' },
+  ];
+
+  const handleTab = (idx) => {
+    setSelect(idx);
+  };
+
+  return (
+    <div className='w-full h-full pt-4'>
+      <div className='w-11/12 h-12 ml-16 bg-zinc-200 flex'>
+        {contents.map((con, idx) => (
+          <div
+            className={`tab ${select === idx ? 'select' : ''}`}
+            onClick={() => {
+              handleTab(idx);
+            }}
+          >
+            {con.name}
+          </div>
+        ))}
+      </div>
+      <div className='h-60 w-full font-semibold text-2xl flex justify-center items-center'>
+        <div>{contents[select].message}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Tab;
