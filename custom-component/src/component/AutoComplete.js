5d88313063590b3d85760f7cffdc32f3690d7dc9
doyeon <doyeon311@gmail.com>
2022-02-05 22:28:20 +0900

    Feat: AutoComplete

--- custom-component/src/component/AutoComplete.js
+++ custom-component/src/component/AutoComplete.js
@@ -1,7 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const data = [
+  'antique',
+  'refurbished',
+  'vintage',
+  'rustic',
+  '중고A급',
+  '중고B급',
+];
 
 const AutoComplete = () => {
-  return <></>;
+  const [isShow, setIsShow] = useState(false);
+  const [text, setText] = useState('');
+  const [wordList, setWordList] = useState([]);
+
+  useEffect(() => {
+    const searchWord = () => {
+      const words = data.filter(
+        (word) =>
+          word.includes(text.toUpperCase()) || word.includes(text.toLowerCase())
+      );
+      setWordList(words);
+    };
+    if (text === '') {
+      setIsShow(false);
+    } else {
+      setIsShow(true);
+      searchWord();
+    }
+  }, [text]);
+
+  const changeText = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div className='w-full h-full flex flex-col items-center justify-center'>
+      <div className='w-full flex flex-col focus-within:shadow-md rounded-xl'>
+        <div
+          className={`h-12 flex py-2 px-3 border-neutral-300 border-2 rounded-xl ${
+            text.length > 0 && wordList.length > 0 ? 'rounded-b-none' : ''
+          }`}
+        >
+          <input
+            type='text'
+            className='flex flex-grow outline-none'
+            value={text}
+            onChange={changeText}
+          />
+          <span
+            className='font-semibold cursor-pointer'
+            onClick={() => setText('')}
+          >
+            x
+          </span>
+        </div>
+        <div className='relative'>
+          {isShow && wordList.length > 0 ? (
+            <ul className='absolute w-full bg-white border-neutral-300 border-t-0 border-2 rounded-b-xl shadow-md'>
+              {wordList.map((word, idx) => (
+                <li
+                  key={idx}
+                  className='px-3 my-2 hover:bg-neutral-200'
+                  onClick={() => setText(word)}
+                >
+                  {word}
+                </li>
+              ))}
+            </ul>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AutoComplete;
